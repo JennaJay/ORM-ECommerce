88d284fe29c3dd57bc7e82b1c0cf042dff1ab0f8
Jenna Jay <jenneh83@gmail.com>
2024-02-06 21:35:33 -0600

    Added routes for Tag api and index

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,14 +5,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all products be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model: Category}, {model: Tag}]
+  })
+  .then((product) => {
+    res.status(200).json(product)
+  }) .catch((err) => {
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // find a single product by its `id`be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag}]
+  }) .then((product) => {
+    res.status(200).json(product)
+  }) .catch((err) => {
+    res.status(500).json(err)
+  })
 });
 
 // create new product
@@ -94,6 +107,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  }) .then((product) => {
+    res.status(200).json(product)
+  }) .catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,8 +4,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags be sure to include its associated Product data
+Tag.findAll({
+  include: [{model: Product}]
+}) .then((tag) => {
+  res.status(200).json(tag)
+}) .catch((err) => {
+  res.status(500).json(err)
+})
 });
 
 router.get('/:id', (req, res) => {
